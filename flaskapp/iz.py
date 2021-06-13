89bb50798233f1ff3bb7e36b9f7ecf2b76c1dc26
Mishelle19 <84457541+Mishelle19@users.noreply.github.com>
2021-06-14 00:59:27 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-return " <html><head></head> <body> To add: /net. </body></html>"
+  return " <html><head></head> <body> To add: /net. </body></html>"
 
 from flask import render_template
 from flask_wtf import FlaskForm,RecaptchaField
@@ -20,64 +20,47 @@ app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
 from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)
 class NetForm(FlaskForm):
-openid = StringField('Изменить: 0 - по вертикали, 1 - по горизонтали.', validators = [DataRequired()])
-upload = FileField('Загрузка изображения', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
-recaptcha = RecaptchaField()
-submit = SubmitField('send')
-from werkzeug.utils import secure_filename
-import os
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-import seaborn as sns
+  openid = StringField('Изменить: 0 - по вертикали, 1 - по горизонтали.', validators = [DataRequired()])
+  upload = FileField('Загрузка изображения', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+  recaptcha = RecaptchaField()
+  submit = SubmitField('send')
+ from werkzeug.utils import secure_filename
+ import os
+ import numpy as np
+ from PIL import Image
+ import matplotlib.pyplot as plt
+ import seaborn as sns
 
 def draw(filename,openid):
-print(filename)
-imag= Image.open(filename)
-x, y = imag.size
-openid=int(openid)
+  print(filename)
+  imag= Image.open(filename)
+  x, y = imag.size
+  openid=int(openid)
 
-fig = plt.figure(figsize=(6, 4))
-ax = fig.add_subplot()
-data = np.random.randint(0, 255, (100, 100))
-ax.imshow(imag, cmap='plasma')
-b = ax.pcolormesh(data, edgecolors='black', cmap='plasma')
-fig.colorbar(b, ax=ax)
-gr_path = "./static/newgr.png"
-sns.displot(data)
-plt.savefig(gr_path)
-plt.close()
-
-if openid==0:
-a = imag.crop((0, 0, int(y * 0.5), x))
-b = imag.crop((int(y * 0.5), 0, x, y))
-imag.paste(b, (0, 0))
-imag.paste(a, (int(x * 0.5), 0))
-output_filename = filename
-imag.save(output_filename)
-if openid==1:
-imag=imag.rotate(90)
-a = imag.crop((0, 0, int(y * 0.5), x))
-b = imag.crop((int(y * 0.5), 0, x, y))
-imag.paste(b, (0, 0))
-imag.paste(a, (int(y * 0.5), 0))
-imag=imag.rotate(270)
-output_filename = filename
-imag.save(output_filename)
+  fig = plt.figure(figsize=(6, 4))
+  ax = fig.add_subplot()
+  data = np.random.randint(0, 255, (100, 100))
+  ax.imshow(imag, cmap='plasma')
+  b = ax.pcolormesh(data, edgecolors='black', cmap='plasma')
+  fig.colorbar(b, ax=ax)
+  gr_path = "./static/newgr.png"
+  sns.displot(data)
+  plt.savefig(gr_path)
+  plt.close()
 return output_filename,gr_path
 
 @app.route("/net",methods=['GET', 'POST'])
 def net():
-form = NetForm()
-filename=None
-newfilename=None
-grname=None
-if form.validate_on_submit():
-filename = os.path.join('./static', secure_filename(form.upload.data.filename))
-ch=form.openid.data
-form.upload.data.save(filename)
-newfilename,grname = draw(filename,ch)
+  form = NetForm()
+  filename=None
+  newfilename=None
+  grname=None
+  if form.validate_on_submit():
+    filename = os.path.join('./static', secure_filename(form.upload.data.filename))
+    ch=form.openid.data
+    form.upload.data.save(filename)
+    newfilename,grname = draw(filename,ch)
 return render_template('net.html',form=form,image_name=newfilename,gr_name=grname)
 
 if __name__ == "__main__":
-app.run(host='127.0.0.1',port=5000)
+  app.run(host='127.0.0.1',port=5000)
