50f4cb8acef91d513408adc36ff55c743e8b2e82
Mishelle19 <84457541+Mishelle19@users.noreply.github.com>
2021-06-13 23:47:36 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -55,7 +55,7 @@ def draw(filename,openid):
 # output_filename = filename
 # imag.save(output_filename)
  
-return gr_path
+return output_filename
 
 @app.route("/net",methods=['GET', 'POST'])
 def net():
